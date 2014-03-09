ab4e7bf0602977a489ae4d9419a2ac08c716906b
Rafael Portela <rafaelportela90@gmail.com>
2014-03-09 00:07:29 -0300

    Activities list ui controller.

--- src/main/webapp/app/js/controllers.js
+++ src/main/webapp/app/js/controllers.js
@@ -2,10 +2,15 @@
 
 /* Controllers */
 
-angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
-
-  }])
-  .controller('MyCtrl2', [function() {
-
-  }]);
+angular.module('narealApp', []).controller('FeedlistController', ['$scope', function($scope) {
+    $scope.activities = [
+        {
+            'title': 'First Activity',
+            'content' : 'I created my first activity, and I am so excited! ;)'
+        },
+        {
+            'title': 'Second Activity',
+            'content' : 'Yeah.. now I am quite familiar with that..'
+        }
+    ];
+}]);
--- src/main/webapp/test/unit/controllersSpec.js
+++ src/main/webapp/test/unit/controllersSpec.js
@@ -2,15 +2,13 @@
 
 /* jasmine specs for controllers go here */
 
-describe('controllers', function(){
-  beforeEach(module('myApp.controllers'));
+describe('controllers', function() {
+  beforeEach(module('narealApp'));
 
+  it('should return activities', inject(function($controller) {
+      var scope = {},
+          controller = $controller('FeedlistController', {$scope: scope});
 
-  it('should ....', inject(function() {
-    //spec body
-  }));
-
-  it('should ....', inject(function() {
-    //spec body
+      expect(scope.activities.length).toBe(2);
   }));
 });
